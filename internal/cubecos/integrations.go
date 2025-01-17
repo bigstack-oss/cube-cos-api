63e875cd62d20e2276ee3431121477ee63ccc1bf
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-01-18 03:36:45 +0800

    [#21] feat(integrations): add a GET api to fetch built-in integrations

---
+++ internal/cubecos/integrations.go
@@ -0,0 +1,44 @@
+package cubecos
+
+import (
+	"fmt"
+
+	definition "github.com/bigstack-oss/cube-cos-api/internal/definition/v1"
+)
+
+var (
+	builtInIntegrations = []definition.Integration{
+		{
+			Name:                    "keycloak",
+			IsHeaderShortcutEnabled: true,
+			Description:             "Keycloak Dashboard",
+			IsBuiltIn:               true,
+			Url:                     fmt.Sprintf("https://%s:10443/auth/admin", definition.ControllerVip),
+		},
+		{
+			Name:                    "openstack",
+			IsHeaderShortcutEnabled: true,
+			Description:             "OpenStack Dashboard",
+			IsBuiltIn:               true,
+			Url:                     fmt.Sprintf("https://%s:9999", definition.ControllerVip),
+		},
+		{
+			Name:                    "rancher",
+			IsHeaderShortcutEnabled: true,
+			Description:             "Rancher Dashboard",
+			IsBuiltIn:               true,
+			Url:                     fmt.Sprintf("https://%s:10443", definition.ControllerVip),
+		},
+		{
+			Name:                    "ceph",
+			IsHeaderShortcutEnabled: true,
+			Description:             "Ceph Dashboard",
+			IsBuiltIn:               true,
+			Url:                     fmt.Sprintf("https://%s/ceph", definition.ControllerVip),
+		},
+	}
+)
+
+func ListBuiltInIntegrations() []definition.Integration {
+	return builtInIntegrations
+}
