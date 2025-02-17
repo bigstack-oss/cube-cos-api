0bfa8f8b5be7904e2cc1aaa2d485e7cafa7f8b6b
arashi87 <arashi.li@bigstack.co>
2025-02-17 13:50:04 +0800

    feat(settings): implement the GET api to retrieve the email sender

--- internal/api/v1/settings/handlers.go
+++ internal/api/v1/settings/handlers.go
@@ -1,10 +1,13 @@
 package settings
 
 import (
+	"errors"
+
 	"github.com/bigstack-oss/cube-cos-api/internal/api"
 	v1 "github.com/bigstack-oss/cube-cos-api/internal/definition/v1"
 	"github.com/gin-gonic/gin"
 	log "go-micro.dev/v5/logger"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var Handlers = []api.Handler{
@@ -13,6 +16,12 @@ var Handlers = []api.Handler{
 		Method:  "POST",
 		Path:    "/settings/email-sender",
 		Func:    createEmailSender,
+	},
+	{
+		Version: api.V1,
+		Method:  "GET",
+		Path:    "/settings/email-sender",
+		Func:    getEmailSender,
 	},
 }
 
@@ -30,4 +39,19 @@ func createEmailSender(c *gin.Context) {
 	}
 
 	api.SetStatusCreated(c, "email sender created successfully", nil)
+}
+
+func getEmailSender(c *gin.Context) {
+	emailSender, err := getEmailSenderRecord()
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		api.SetStatusNotFound(c, errors.New("email sender not found"))
+		return
+	}
+	if err != nil {
+		log.Errorf("request(%s): failed to get email sender: %s", api.GetReqId(c), err.Error())
+		api.SetInternalServerError(c, err)
+		return
+	}
+
+	api.SetStatusOk(c, "email sender retrieved successfully", emailSender)
 }
--- internal/api/v1/settings/record.go
+++ internal/api/v1/settings/record.go
@@ -24,3 +24,18 @@ func upsertEmailSenderRecord(emailSender v1.EmailSender) error {
 	}
 	return nil
 }
+
+func getEmailSenderRecord() (v1.EmailSender, error) {
+	h := cubeMongo.GetGlobalHelper()
+	emailSender := v1.EmailSender{}
+	res, err := h.Get(v1.SettingsDB(), emailSender.Collection(), bson.M{"deleted": bson.M{"$ne": true}})
+	if err != nil {
+		log.Errorf("failed to get email sender record (%s)", err.Error())
+		return emailSender, err
+	}
+	if err := res.Decode(&emailSender); err != nil {
+		log.Errorf("failed to decode email sender record (%s)", err.Error())
+		return emailSender, err
+	}
+	return emailSender, nil
+}
