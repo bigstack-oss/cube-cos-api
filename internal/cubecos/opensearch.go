72c642ee45bcffc1ac219e658d844eb8f531415d
bjergsen zhu <bjergsen.zhu@bigstack.co>
2025-09-29 12:09:10 +0800

    fix(fqdn): add the domain switching for opensearch link

--- internal/cubecos/opensearch.go
+++ internal/cubecos/opensearch.go
@@ -2,8 +2,11 @@ package cubecos
 
 import (
 	"fmt"
+	"net/url"
 	"os/exec"
 	"strings"
+
+	"github.com/bigstack-oss/cube-cos-api/internal/definition/v1/base"
 )
 
 func GetOpenSearchRequestLink(requestId string) (string, error) {
@@ -17,5 +20,17 @@ func GetOpenSearchRequestLink(requestId string) (string, error) {
 	}
 
 	link := string(out)
-	return strings.Trim(link, "\n"), nil
+	u, err := url.Parse(link)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse opensearch request link: %s (%v)", link, err)
+	}
+
+	segments := strings.Split(u.Host, ":")
+	if len(segments) < 1 {
+		return "", fmt.Errorf("invalid opensearch request link: %s", link)
+	}
+
+	segments[0] = base.DataCenterVip
+	u.Host = strings.Join(segments, ":")
+	return url.QueryUnescape(u.String())
 }
