b5be917b343a853d0c2faf5b80a2565ca618898f
raven-pan-bigstack <raven.pan@bigstack.co>
2026-06-16 16:12:19 +0800

    fix: nvml fatal error on gpu not supported server
    Signed-off-by: raven-pan-bigstack <raven.pan@bigstack.co>

--- internal/runtime/nvml.go
+++ internal/runtime/nvml.go
@@ -12,8 +12,18 @@ func initNvml() error {
 	ret := nvml.Init()
 	if ret != nvml.SUCCESS {
 		errorString := nvml.ErrorString(ret)
+
+		if ret == nvml.ERROR_DRIVER_NOT_LOADED {
+			log.Warnf("Failed to initialize NVML: %v", errorString)
+			log.Warnf("NVIDIA driver is not loaded. Continuing without NVML support, GPU features will be unavailable.")
+
+			return nil
+		}
+
 		log.Fatalf("Failed to initialize NVML: %v", errorString)
+
 		return errors.New(errorString)
 	}
+
 	return nil
 }
